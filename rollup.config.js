9dd09f878f2d64a984c4c16e0fa2ce239f5e91ce
syJSdev <whitedragon226@gmail.com>
2022-06-08 19:42:36 +0200

    upgrade the packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,80 +1,79 @@
+import path from "node:path";
+import { babel } from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+// import eslint from "@rollup/plugin-eslint";
+import { nodeResolve as resolve } from "@rollup/plugin-node-resolve";
 import filesize from "rollup-plugin-filesize";
-import { eslint } from "rollup-plugin-eslint";
-import babel from "rollup-plugin-babel";
-import commonjs from "rollup-plugin-commonjs";
-import resolve from "rollup-plugin-node-resolve";
 import sass from "rollup-plugin-sass";
-import { uglify } from "rollup-plugin-uglify";
+import { terser } from "rollup-plugin-terser";
+import autoprefixer from "autoprefixer";
+import postcss from "postcss";
 import pkg from "./package.json";
 
+const plugins = [
+  resolve({
+    browser: true,
+    preferBuiltins: true,
+  }),
+  commonjs({
+    exclude: "node_modules/process-es6/**",
+    include: "node_modules/**",
+  }),
+  babel({ babelHelpers: "bundled", configFile: path.resolve(__dirname, ".babelrc") }),
+  filesize(),
+];
+
 // eslint-disable-next-line import/no-anonymous-default-export
-export default [{
-  input: pkg.src,
-  output: {
-    format: "iife",
-    "name": "Tourguide",
-    "strict": true,
-    file: pkg.main
+export default [
+  {
+    input: pkg.src,
+    output: [
+      {
+        format: "esm",
+        exports: "auto",
+        file: pkg.module,
+      },
+      {
+        format: "umd",
+        name: "Tourguide",
+        file: pkg.main,
+      },
+    ],
+    plugins: [
+      sass({
+        insert: false,
+        output: "tourguide.css",
+        options: { outputStyle: "compressed" },
+        processor: (css) =>
+          postcss([autoprefixer])
+            .process(css, { from: undefined })
+            .then((result) => result.css),
+      }),
+      // eslint({
+      //   include: ["./src/**"],
+      //   fix: true,
+      //   throwOnError: true,
+      // }),
+      ...plugins,
+    ],
   },
-  plugins: [
-    eslint({
-      include: [
-        "./src/**",
-      ]
-    }),
-    sass({ output: "tourguide.css", options: { outputStyle: "compressed" } }),
-    resolve({ mainFields: ["module", "jsnext:main", "browser"], browser: true, preferBuiltins: true }),
-    commonjs({
-      exclude: "node_modules/process-es6/**",
-      include: pkg.commonjs
-    }),
-    babel({
-      exclude: pkg.commonjs
-    }),
-    filesize(),
-  ]
-}, {
-  input: pkg.main,
-  output: {
-    format: "esm",
-    file: pkg.main.replace(".js", ".min.js")
+  {
+    input: pkg.src,
+    output: [
+      {
+        format: "iife",
+        strict: false,
+        exports: "auto",
+        name: "Tourguide",
+        file: pkg.browser,
+      },
+    ],
+    plugins: [
+      sass({
+        insert: false,
+      }),
+      ...plugins,
+      terser(),
+    ],
   },
-  plugins: [uglify()]
-}, {
-  input: pkg.src,
-  output: {
-    format: "esm",
-    file: pkg.module
-  },
-  plugins: [
-    sass({ insert: false }),
-    resolve({ mainFields: ["module", "jsnext:main", "node"], browser: true, preferBuiltins: true }),
-    commonjs({
-      exclude: "node_modules/process-es6/**",
-      include: pkg.commonjs
-    }),
-    babel({
-      exclude: pkg.commonjs
-    }),
-    filesize(),
-  ]
-}, {
-  input: pkg.src,
-  output: {
-    format: "umd",
-    "name": "Tourguide",
-    file: pkg.module.replace(".esm.js", ".umd.js")
-  },
-  plugins: [
-    sass({ insert: false }),
-    resolve({ mainFields: ["module", "jsnext:main", "node"], browser: true, preferBuiltins: true }),
-    commonjs({
-      exclude: "node_modules/process-es6/**",
-      include: pkg.commonjs
-    }),
-    babel({
-      exclude: pkg.commonjs
-    }),
-    filesize(),
-  ]
-}];
+];
